a7c8ddc99a76fde4cb1cef5393d8c973be2ab1f3
Alvaro <alvaro.monserrat@gmail.com>
2020-03-06 17:26:42 -0300

    Update Clase Employee

---
+++ JavaTutorial/src/com/javatutorial/JavaTutorial.java
@@ -0,0 +1,15 @@
+package com.javatutorial;
+
+
+public class JavaTutorial {
+	public static void main(String [] args) {
+		
+		Employee worker = new Employee("Pedro");
+		
+		worker.setAge(25);
+
+		worker.printEmployee();
+		
+		
+	}
+}
--- JavaTutorial/src/com/javatutorial/javaTutorial.java
+++
@@ -1,7 +0,0 @@
-package com.javatutorial;
-
-public class javaTutorial {
-	public static void main(String [] args) {
-		System.out.println("Hello");
-	}
-}
