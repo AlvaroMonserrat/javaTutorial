265de477b0c1129664f1375870848692d292c27c
Alvaro <alvaro.monserrat@gmail.com>
2020-03-13 18:23:55 -0300

    Fechas y objectos contenedores: Set y ListArray

---
+++ JavaTutorial/src/com/javatutorial/DataDemo.java
@@ -0,0 +1,12 @@
+package com.javatutorial;
+import java.util.Date;
+
+public class DataDemo {
+	
+	Date date = new Date();
+
+	public String datePrint(){
+		return date.toString();
+	}
+	
+}
--- JavaTutorial/src/com/javatutorial/JavaTutorial.java
+++ JavaTutorial/src/com/javatutorial/JavaTutorial.java
@@ -1,27 +1,47 @@
 package com.javatutorial;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class JavaTutorial {
 	public static void main(String [] args) {
 		
-		Employee worker = new Employee("Pedro");
+		Employee secretary = new Employee("Pedro");
+		secretary.setAge(25);
 		
-		worker.setAge(25);
-
-		worker.printEmployee();
+		Employee engineer = new Employee("Maria");
+		engineer.setAge(28);
 		
-		Dia dia;
-		dia = Dia.LUNES;
+		Set<Employee> conjunto = new HashSet<Employee>();
+		conjunto.add(secretary);
+		conjunto.add(engineer);
 		
-		System.out.println(dia);
-		System.out.println(dia.name());
-		System.out.println(dia.ordinal());
+		for(Employee r: conjunto) {
+			r.printEmployee();
+		}
+		
+		ArrayList<Employee> lista = new ArrayList<Employee>();
+		lista.add(new Employee("Alvaro"));
+		lista.add(new Employee("Paly"));
+		lista.add(new Employee("Pato"));
+		lista.add(new Employee("Gato"));
+		
+		for(Employee element: lista) {
+			System.out.println(element.getName());
+		}
 		
 		double [] arrayNumbers = {2, 4, 5, 10, 1};
 		TestArray nArray = new TestArray(arrayNumbers);
-		
 		System.out.println(nArray.sumAllElement());
 		System.out.println(nArray.maxElements());
 		System.out.println(nArray.toString());
+		
+		DataDemo newDate = new DataDemo();
+		System.out.println(newDate.datePrint());
+		
+
+		
 	}
 }
